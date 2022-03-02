f21ae9666e725ec3139c68f8f7ca6bcd4b5f0c4c
alexiaruizq <53828050+alexiaruizq@users.noreply.github.com>
2022-03-01 20:49:11 -0700

    Add files via upload

---
+++ CovidEncuesta/app/Http/Controllers/Api/TiposUsuarioController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\TipoUsuario;
+
+class TiposUsuarioController extends Controller
+{
+    public function index () {
+        $tiposUsuario = TipoUsuario::all();
+        return $tiposUsuario;
+    }
+}
